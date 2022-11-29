74511cba5806812abaf80ef8d2609a907492d572
Isela Alarcón <isela.v.alarcon@gmail.com>
2022-11-29 13:28:31 +0100

    working woth classes in a modal window

--- starter/script.js
+++ starter/script.js
@@ -3,8 +3,53 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnsOpenModal = document.querySelectorAll('show-modal');
-console.log(btnsOpenModal);
+const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].addEventListener('click'), function () {});
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+/*Creamos una función para cerrar si el usuario da click dentro del pop up*/
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+/* Es lo mismo que: 
+  modal.style.display = 'block';
+  */
+
+btnCloseModal.addEventListener('click', closeModal);
+
+/*Al dar click en el overlay afuera del modal se cierra el pop up */
+overlay.addEventListener('click', closeModal);
+
+/* Usando  spread operator y map*/
+// [...btnsOpenModal].map((button, i) =>
+//   button.addEventListener('click', function () {
+//     console.log(`button clicked, ${i}`);
+//   })
+// );
+
+/* usando map y object values para convetir un array */
+// Object.values(btnsOpenModal).map((button, i) =>
+//   button.addEventListener('click', function () {
+//     console.log(`button clicked, ${i}`);
+//   })
+// );
+
+// const user = {
+//   name: 'isela',
+//   age: 23,
+//   size: 47,
+// };
+
+// console.log(user);
+// console.log(Object.values(user));
+// console.log(Object.keys(user));
+// console.log(Object.entries(user));
